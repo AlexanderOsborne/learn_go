4c2731e3dfb27ab064ce7bafe7f97df0c8f2e4a1
awosborn <alexander.w.osborne@gmail.com>
2021-04-02 17:00:59 -0600

    Hello handles english or spanish greeting

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -2,34 +2,6 @@ package main
 
 import "testing"
 
-// func TestHello(t *testing.T) {
-// //   got := Hello("Chris")
-// //   want := "Hello, Chris"
-
-// //   if got != want {
-// //     t.Errorf("got %q want %q", got, want)
-// //   }
-// // }
-
-// t.Run("saying hello to people", func(t *testing.T) {
-// 	got := Hello("Chris")
-// 	want := "Hello, Chris"
-
-// 	if got != want {
-// 		t.Errorf("got %q want %q", got, want)
-// 	}
-// })
-
-// t.Run("say, 'Hello, World' when a an empty string is applied", func(t *testing.T) {
-// 	got := Hello("")
-// 	want := "Hello, World"
-
-// 	if got != want {
-// 		t.Errorf("got %q want %q", got, want)
-// 	}
-// })
-// }
-
 func TestHello(t *testing.T) {
 
 	assertCorrectMessage := func(t testing.TB, got, want string) {
@@ -40,15 +12,21 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-			got := Hello("Chris")
+			got := Hello("Chris","")
 			want := "Hello, Chris"
 			assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string defaults to 'World'", func(t *testing.T) {
-			got := Hello("")
+			got := Hello("","")
 			want := "Hello, World"
 			assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In Spanish", func(t *testing.T) {
+		got := Hello("Diana", "Spanish")
+		want := "Hola, Diana"
+		assertCorrectMessage(t, got, want)
 	})
 
 }
