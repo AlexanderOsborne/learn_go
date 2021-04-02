6eafb25b27d4dc953e9e9aaf8d078e0754051b01
awosborn <alexander.w.osborne@gmail.com>
2021-04-02 12:12:29 -0600

    Hello world and test

---
+++ hello_world/hello.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// func main() {
+// 	fmt.Println("Hello World")
+// }
+
+func Hello() string {
+	return "Hello, World"
+}
+
+func main() {
+	fmt.Println(Hello())
+}
